b76d3ddb8782f6c99a89fb6bb670238e02823b81
sugihara <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-09-17 14:14:49 +0900

    update translate script

--- translate_readme.py
+++ translate_readme.py
@@ -1,10 +1,34 @@
+import markdown
+from bs4 import BeautifulSoup
 from googletrans import Translator
 
+# README.mdを読み込む
 with open('README.md', 'r', encoding='utf-8') as file:
     readme_content = file.read()
 
-translator = Translator()
-translated = translator.translate(readme_content, src='en', dest='ja').text
+# MarkdownをHTMLに変換
+html_content = markdown.markdown(readme_content)
 
+# BeautifulSoupを使ってHTMLからテキストを抽出しつつ、タグは保持
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# 翻訳インスタンスの生成
+translator = Translator()
+
+def translate_element(element):
+    """タグ内のテキストを翻訳する"""
+    if element.string:
+        translated = translator.translate(element.string, src='en', dest='ja').text
+        element.string.replace_with(translated)
+
+# HTMLの各要素を再帰的に翻訳
+for elem in soup.find_all(text=True):
+    if elem.parent.name not in ['code', 'pre']:  # コードブロックは翻訳しない
+        translate_element(elem)
+
+# HTMLをMarkdownに戻す（基本的には可能な限りフォーマットを保つ）
+translated_markdown = str(soup)
+
+# 翻訳結果をREADME.ja.mdとして保存
 with open('README.ja.md', 'w', encoding='utf-8') as file:
-    file.write(translated)
+    file.write(translated_markdown)
